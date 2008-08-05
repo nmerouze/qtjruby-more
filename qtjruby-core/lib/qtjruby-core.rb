5c6f963475102084293b6d8fdbb1fb010860cec5
Nicolas Mérouze <nicolas.merouze@gmail.com>
2008-08-05 12:10:36 +0200

    Remove custom gem path.

--- qtjruby-core/lib/qtjruby-core.rb
+++ qtjruby-core/lib/qtjruby-core.rb
@@ -2,19 +2,3 @@ require 'java'
 require 'qtjruby-core.jar'
 require 'qtjruby-core/core_ext'
 require 'qtjruby-core/qt'
-
-module Qt
-  module JRuby
-    
-    @@root = nil
-    
-    class << self
-      
-      def root
-        @@root ||= Dir.pwd
-      end
-      
-    end
-    
-  end
-end
