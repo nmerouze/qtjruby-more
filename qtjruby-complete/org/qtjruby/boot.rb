8ec05181c6c05369c567f00dba864ea37ed5e376
Nicolas Mérouze <nicolas.merouze@gmail.com>
2008-08-28 09:35:12 +0200

    Update README and modify the installation gem method.

--- qtjruby-complete/org/qtjruby/boot.rb
+++ qtjruby-complete/org/qtjruby/boot.rb
@@ -2,7 +2,7 @@ file_path = ARGV.shift
 root_path = File.dirname(file_path)
 root_path = Dir.pwd unless File.exist?(root_path + '/gems')
 
-Dir.glob(root_path + '/gems/gems/*/lib') { |lib| $:.push(lib) }
+Dir.glob(root_path + '/gems/*/lib') { |lib| $:.push(lib) }
 Dir.glob(root_path + '/jars/*') { |jar| require jar }
 
 require 'qtjruby-core'
